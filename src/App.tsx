9a493770235be1c8bea0f4f16b31d734ce5149d3
MauricioAires <mauricioaires015@gmail.com>
2023-01-10 00:37:10 -0300

    feat: config router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+
+import { Router } from './Router'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <h1>Hello</h1>
-      <button>xama</button>
-
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
