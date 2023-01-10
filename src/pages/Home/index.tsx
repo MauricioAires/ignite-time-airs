0fcc4682c8dfd0b8650f5d6499cd2c3f7f9e230a
MauricioAires <mauricioaires015@gmail.com>
2023-01-10 09:49:50 -0300

    feat: home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import { Play } from 'phosphor-react'
+
+import * as S from './styles'
+
+export function Home() {
+  return (
+    <S.HomeContainer>
+      <form>
+        <S.FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" type="text" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input id="minutesAmount" type="number" />
+        </S.FormContainer>
+
+        <S.CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <S.Separator>:</S.Separator>
+          <span>0</span>
+          <span>0</span>
+        </S.CountdownContainer>
+
+        <button>
+          <Play size={24} /> Começar
+        </button>
+      </form>
+    </S.HomeContainer>
+  )
+}
