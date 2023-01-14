5b7071e2633111d2cec0515aa5649014a6424e61
MauricioAires <mauricioaires015@gmail.com>
2023-01-14 11:04:03 -0300

    feat: counter history

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -31,7 +31,7 @@ export function NewCycleForm() {
         type="number"
         placeholder="00"
         step={5}
-        min={1}
+        min={0}
         max={60}
         {...register('minutesAmount', {
           valueAsNumber: true,
